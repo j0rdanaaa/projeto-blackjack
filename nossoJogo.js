0b28667e5b5164311d9c2bd663778327ea7b1d41
j0rdanaaa <j0rdanaaa@gmail.com>
2023-03-03 20:40:42 -0300

    nosso jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,36 @@
  * 
  * 
  */
+
+    console.log("Bem-vindo(a) ao jogo de blackjack!")
+
+const jogo = confirm("Quer iniciar uma nova rodada?")
+if( jogo === true) 
+    console.log("Iniciando uma nova rodada")
+   
+
+
+if (jogo) {
+    const carta1User = comprarCarta()
+    const carta2User = comprarCarta()
+    const carta1Pc = comprarCarta()
+    const carta2Pc = comprarCarta()
+
+    let pontuacaoUsuario = carta1User.valor + carta2User.valor
+    let pontuacaoPc = carta1Pc.valor + carta2Pc.valor
+
+    console.log(`Usuário - cartas: ${carta1User.texto} ${carta2User.texto} - ${pontuacaoUsuario}`)
+    console.log(`Computador - cartas: ${carta1Pc.texto} ${carta2Pc.texto} - ${pontuacaoPc}`)
+
+    if (pontuacaoUsuario => 21) {
+        console.log("O usuário ganhou!")
+    } else if (pontuacaoPc => 21) {
+        console.log("O computador ganhou!")
+    } else if (pontuacaoUsuario === pontuacaoPc) {
+        console.log("Empate!")
+    }
+
+} else {
+    console.log("O jogo acabou")
+}
+
